2e90c11ff594405810a325f7e8e3022a89c2a0de
Ancel <ancel.ajanga@student.moringaschool.com>
2021-06-14 08:21:15 +0300

    Add javascript changes

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ function naming(){
     var YY=parseInt(document.getElementById("name").value);
     var MM=parseInt(document.getElementById("Form").value);
     var DD=parseInt(document.getElementById("akan").value);
-    var day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sartuday"];
-    var femaleNames=["Akosua", "Adwoa","Abenaa","Akua","Yaa"," Afua","Ama"];
-    var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var day=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+    var femaleNames=["Adwoa" "Abenaa","Akua","Yaa"," Afua","Ama", "Akosua"];
+    var maleNames=["Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame", "Kwasi"];
     if(CC.value =="" || YY.value =="" || MM.value =="" || DD.value ==""){
         alert("No blank values allowed");
     }
